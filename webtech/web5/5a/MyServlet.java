eaddafb25ac6b401e7581917d7b9838038035a12
Hiya-Divya <150521096+Hiya-Divya@users.noreply.github.com>
2024-11-26 09:20:28 +0530

    Create MyServlet.java

---
+++ webtech/web5/5a/MyServlet.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class MyServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, this is MyServlet!</h1>");
+    }
+}
